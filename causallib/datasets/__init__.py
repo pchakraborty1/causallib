51d87e3144e4565ad619cfa05115bc114b5f3b8c
Prithwish Chakraborty <prithwish.chakraborty@ibm.com>
2022-08-23 17:43:31 -0500

    adding loader scripts for synthetic data set

--- causallib/datasets/__init__.py
+++ causallib/datasets/__init__.py
@@ -1,2 +1,2 @@
-from .data_loader import load_nhefs, load_nhefs_survival, load_acic16
+from .data_loader import load_nhefs, load_nhefs_survival, load_acic16, load_synth_dynamic_treat
 from ..simulation.CausalSimulator3 import CausalSimulator3 as CausalSimulator, generate_random_topology
--- causallib/datasets/data_loader.py
+++ causallib/datasets/data_loader.py
@@ -157,6 +157,44 @@ def load_nhefs_survival(augment=True, onehot=True):
     return X, a, t, y
 
 
+def load_synth_dynamic_treat(observational=True):
+    """
+    Loads and pre-processes the synthetic dynamic treatment dataset.
+
+    See https://github.com/pchakraborty1/causallib/blob/test_cases_issue_1/causallib/datasets/data/synthetic_dynamic_treatment/README.md
+    for details on how data was generated
+
+    Args:
+        observational (bool): which strategy to load
+                    If True, loads the train, validation, and test folds for the observational strategy
+                    If False, loads the never treat strategy data. This is only test
+
+    Returns:
+        Dictionary with folds as keys. each fold contains
+        X (pd.DataFrame): covariate matrix of size ((num_subjects x num_timepoints), num_features).
+        a (pd.Series): Treatment assignment of size ((num_subjects x num_timepoints),). treatment dose
+    """
+    dir_name = os.path.join(DATA_DIR_NAME, "synthetic_dynamic_treatment")
+
+    paylod = dict()
+    expt_name = 'simx2'
+    if observational:
+        sections = ['train', 'val', 'test']
+        for s in sections:
+            fName = f"{expt_name}_observational_{s}.csv"
+            data = load_data_file(fName, dir_name)
+            X = data[['id', 'time', 'cov1', 'cov2']].set_index(['id', 'time'])
+            a = data[['id', 'time', 'treatment']].set_index(['id', 'time'])
+            payload[s] = (X, a)
+    else:
+        fName = f"{expt_name}_never-treat.csv"
+        data = load_data_file(fName, dir_name)
+        X = data[['id', 'time', 'cov1', 'cov2']].set_index(['id', 'time'])
+        a = data[['id', 'time', 'treatment']].set_index(['id', 'time'])
+        payload['test'] = (X, a)
+    return payload
+
+
 def load_acic16(instance=1, raw=False):
     """ Loads single dataset from the 2016 Atlantic Causal Inference Conference data challenge.
 
